547aca93102ab9dbafb3aeace291e519e487e7f7
Joey Simone <joseph@thesimones.us>
2020-11-02 22:54:25 -0800

    I've now added the bulk of the code from the example book, but as you can see, I used some copy-pasting, so it's far from done

--- guessinggame.cpp
+++ guessinggame.cpp
@@ -4,7 +4,8 @@
 #include <string>
 using namespace std;
 
-	int main() {
+	int main() 
+	{
 		unsigned int seed = time(0);
 		int compNum, userGuess;
 		string name;
@@ -12,4 +13,28 @@ using namespace std;
 		srand(seed);
 		compNum = rand() % 100 + 1;
 
+		cout <<"What is your name?" << endl;
+		getline(cin, name);
+		cout <<"Hello, " <<name << endl;
 
+		do
+		{
+			cout <<I'm thinking of a number 1-100. Guess what it is?" <<endl;
+			cin >> userGuess;
+			while (userGuess <=0 || userGuess > 100)
+			{
+				cout << "Invalid Guess. Choose between 1-100" << endl;
+				cin >> userGuess;
+			}
+			if(compNum < userGuess)
+				cout <<"Too high, try again!" <<endl;
+			if(compNum < userGuess)
+				cout <<"Too high, try again!" <<endl;
+			if(compNum < userGuess)
+			{
+				cout<<"Nice work! My number " << compNum << "matches your guess " << userGuess << endl;
+				cout <<"You Win!" << endl;
+			}
+		} while (compNum != userGuess);
+		return 0
+	}
